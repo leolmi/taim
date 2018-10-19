e533d0b807410e1e7ff3f597d94a08280fa4b9fc
leolmi <leo.olmi@gmail.com>
2018-10-19 17:28:43 +0200

    initial commit

--- app.js
+++ app.js
@@ -3,9 +3,14 @@
    * TAIM
    */
 
+  const default_line_rgx = /(\d+\/\d+\/\d+).*?(\d+).*?(\d+).*?(\w+)/gmi;
 
+  const lines = document.getElementById('lines');
+  const line = document.getElementById('line-template').innerHTML;
 
-
+  w.addLine = function(e) {
+    lines.innerHTML += '<br>\n'+line;
+  };
 
 
 })(this);
